1316e88e3bcd94e8ede79e902291b93d899fdea6
Jas Eckard <jas+git@eckard.com>
2016-01-13 23:57:39 -0500

    Added includes; open(), close()

--- lab1/useKyouko3.c
+++ lab1/useKyouko3.c
@@ -11,15 +11,21 @@
 * This file is ...
 */
 
-#define DEVFNAME "/dev/kyouko3"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "kyouko3.h"
 
 int main(int argc, char **argv)
 {
-        dev_t dev = makedev(MAJOR, MINOR);
+        dev_t dev = makedev(MKNOD_MAJOR, MKNOD_MINOR);
 
         mknod(DEVFNAME, S_IFCHR, dev);
 
+        int fd = open(DEVFNAME, O_RDWR);
 
+        close(fd);
 
         return 0;
 }
