1d1a41dfd9c883577c203e1f9c35880b690876bc
Jas Eckard <jas+git@eckard.com>
2016-01-13 23:25:14 -0500

    pulled out common macros

---
+++ lab1/kyouko3.h
@@ -0,0 +1,21 @@
+/**
+* @file kyouko3.h
+* @author Jas Eckard <jas@eckard.com>
+*
+* @section LICENSE
+*
+* (GPL license?)
+*
+* @section DESCRIPTION
+*
+* This file is ...
+*/
+
+#ifndef kyouko3_h
+#define kyouko3_h
+
+#define DEVFNAME "/dev/kyouko3"
+#define MKNOD_MAJOR 500
+#define MKNOD_MINOR 127
+
+#endif  // kyouko3_h
